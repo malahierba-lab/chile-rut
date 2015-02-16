15205e234936e9729c94af9dbe8648885d8d2720
Cristian Santana <hola@cristiansantana.cl>
2015-02-16 10:47:04 -0300

    Se agrega la posibilidad de generar el digito verificador a partir del rut.

--- src/MalahierbaLab/ChileRut/ChileRut.php
+++ src/MalahierbaLab/ChileRut/ChileRut.php
@@ -28,20 +28,96 @@ class ChileRut {
 		$cleanedRut = $this->clean($rut);
 		
 		if (! $cleanedRut)
-			return FALSE;
+			return false;
 		
 		list($numero, $digitoVerificador) = explode('-', $cleanedRut);
 		
 		//Validamos requisitos mínimos
 		if ((($digitoVerificador != 'K') && (! is_numeric($digitoVerificador))) || (count(str_split($numero)) > 11))
-			return FALSE;
+			return false;
 		
 		//Validamos que todos los caracteres del número sean dígitos
 		foreach(str_split($numero) as $chr) {
 			if (! is_numeric($chr))
-				return FALSE;
+				return false;
 		}
 		
+		//Calculamos el digito verificador
+		$digit = $this->digitoVerificador($numero);
+		
+		//Comparamos el digito verificador calculado con el entregado
+		if($digit == $digitoVerificador)
+			return true;
+		
+		return false;
+	}
+	
+	/**
+	 * Formatea un RUT
+	 *
+	 * Estandariza un rut al formato 11222333-K desde cualquier
+	 * entrada valida como rut Ej.: 11.222.333-k, 11222333k, etc.
+	 * Si la entrada no parece válida retorna FALSE
+	 *
+	 * Si se desea formatear un rut que no incluye dígito verificador,
+	 * se debe establecer el segundo parámetro a false.
+	 *
+	 * @access		public
+	 *
+	 * @param		string $rut
+	 * @param 		boolean $incluyeDigitoVerificador
+	 * @return		string|FALSE
+	 */
+	public function clean($originalRut, $incluyeDigitoVerificador = true) {
+		
+		//Eliminamos espacios al principio y final
+		$originalRut = trim($originalRut);
+
+		//En caso de existir, eliminamos ceros ("0") a la izquierda
+		$originalRut = ltrim($originalRut, '0');
+
+		$input		= str_split($originalRut);
+		$cleanedRut	= '';
+
+		foreach ($input as $key => $chr) {
+
+			//Digito Verificador
+			if ((($key + 1) == count($input)) && ($incluyeDigitoVerificador)){
+				if (is_numeric($chr) || ($chr == 'k') || ($chr == 'K'))
+					$cleanedRut .= '-'.strtoupper($chr);
+				else
+					return false;
+			}
+
+			//Números del RUT
+			elseif (is_numeric($chr))
+					$cleanedRut .= $chr;
+		}
+		
+		if (strlen($cleanedRut) < 3)
+			return false;
+		
+		return $cleanedRut;
+	}
+
+	/**
+	 * Calcula el dígito verificador de un rut
+	 *
+	 * El rut puede ser ingresado en los siguientes formatos:
+	 *
+	 ** xx.xxx.xxx (con separación de miles)
+	 ** xxxxxxxx (sin separación de miles)
+	 *
+	 * @access		public
+	 *
+	 * @param		string $rut
+	 * @return		string|FALSE
+	 */
+	public function digitoVerificador($rut) {
+
+		//Preparamos el RUT recibido
+		$numero = $this->clean($rut, false);
+
 		//Calculamos el dígito verificador
 		$txt		= array_reverse(str_split($numero));
 		$sum		= 0;
@@ -55,59 +131,15 @@ class ChileRut {
 		$b			= 11-$a;
 		
 		if($b == 11)
-			$digit	= 0;
+			$digitoVerificador	= 0;
 		elseif($b == 10)
-			$digit	= 'K';
+			$digitoVerificador	= 'K';
 		else
-			$digit = $b;
+			$digitoVerificador = $b;
 
 		//Convertimos el número a cadena para efectos de poder comparar
-		$digit = (string)$digit;
+		$digitoVerificador = (string)$digitoVerificador;
 
-		
-		if($digit == $digitoVerificador)
-			return TRUE;
-		
-		return FALSE;
-	}
-	
-	/**
-	 * Formatea un RUT
-	 *
-	 * Estandariza un rut al formato 11222333-K desde cualquier
-	 * entrada valida como rut Ej.: 11.222.333-k, 11222333k, etc.
-	 * Si la entrada no parece válida retorna FALSE
-	 *
-	 * @access		public
-	 *
-	 * @param		string $rut
-	 * @return		string|FALSE
-	 */
-	public function clean($originalRut) {
-		
-		//Eliminamos espacios al principio y final
-		$originalRut = trim($originalRut);
-
-		//En caso de existir, eliminamos ceros ("0") a la izquierda
-		$originalRut = ltrim($originalRut, '0');
-
-		$input		= str_split($originalRut);
-		$cleanedRut	= '';
-
-		foreach ($input as $key => $chr) {
-			if (($key + 1) == count($input)){
-				if (is_numeric($chr) || ($chr == 'k') || ($chr == 'K'))
-					$cleanedRut .= '-'.strtoupper($chr);
-				else
-					return FALSE;
-			}
-			elseif (is_numeric($chr))
-					$cleanedRut .= $chr;
-		}
-		
-		if (strlen($cleanedRut) < 3)
-			return FALSE;
-		
-		return $cleanedRut;
+		return $digitoVerificador;
 	}
 }
