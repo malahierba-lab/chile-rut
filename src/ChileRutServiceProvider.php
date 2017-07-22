cea5ed7b73c06a54220ae9291450776c37ad3059
Cristian Santana <hola@cristiansantana.cl>
2017-07-22 19:42:43 -0400

    - Replace deprecated method "shared" for "singleton" in order to register class ChileRut in container as suggested by SebastianBar @GitHub - Change the indent from tabs to 4 spaces

--- src/ChileRutServiceProvider.php
+++ src/ChileRutServiceProvider.php
@@ -4,32 +4,31 @@ use Illuminate\Support\ServiceProvider;
 
 class ChileRutServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['chilerut'] = $this->app->share(function($app)
-		{
-			return new ChileRut;
-		});
-	}
-	
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('chilerut', function(){
+            return new ChileRut;
+        });
+    }
+    
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
 }
